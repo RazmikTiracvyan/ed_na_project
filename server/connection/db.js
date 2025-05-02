4324c970f16028a6ea2cc0870104f2558cbe1bc1
Razmik Tiratsvyan <68191689+RazmikTiracvyan@users.noreply.github.com>
2025-05-02 14:05:57 +0400

    Update db.js

--- server/connection/db.js
+++ server/connection/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/test_project");
+    await mongoose.connect("mongodb+srv://raztiracvyan1:gXSzBMUZejDnoLd5@cluster0.nxtjqp4.mongodb.net/test_project");
     console.log("MongoDB connected successfully!");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
